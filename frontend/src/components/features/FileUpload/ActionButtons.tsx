d9f91ae9bfb866544c0ddff17a4f9016586da505
Nnamdi Okeke <plantingTrees@gmail.com>
2025-03-01 01:31:12 -0500

    completed the drag and drop feature alongside the test cases

--- frontend/src/components/features/FileUpload/ActionButtons.tsx
+++ frontend/src/components/features/FileUpload/ActionButtons.tsx
@@ -1,11 +1,20 @@
 import FileUploadButton from "./FileUploadButton";
 import CreateButton from "./CreateButton";
+import GlobalDragDrop from "../DragandDrop/GlobalDragDrop";
 const ActionButtons = () => {
+  const handleFilesDropped = (files:File[]) => {
+    console.log('Files dropped in upload page: ', files);
+  }
     return (
       <div className="min-h-screen flex items-center justify-center p-8">
         <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
-          <FileUploadButton />
-          <CreateButton />
+          <GlobalDragDrop 
+            onFilesDropped={handleFilesDropped}
+            dropPromptText="Release to Upload"
+            showFileList={true}>
+            <FileUploadButton />
+            <CreateButton />
+          </GlobalDragDrop>
         </div>
       </div>
     );
