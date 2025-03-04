a0fa673195fee41f7672626a1bd52466b49c7d64
Nnamdi Okeke <plantingTrees@gmail.com>
2025-03-04 16:00:47 -0500

    reverted back to original file.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 // App.jsx
+
 import RootLayout from './components/layouts/RootLayout';
 import ActionButtons from './components/features/FileUpload/ActionButtons';
+
 
 export default function App() {
   return (
     <RootLayout>
-      <ActionButtons/>
+        <ActionButtons />
     </RootLayout>
   );
 }
